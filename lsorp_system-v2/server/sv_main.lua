5e78220cb99fa71530fbdca74ac153d9fee5083d
Gerrit Schlinkmann <gerrxt07@proton.me>
2023-12-04 21:21:03 +0100

    Added update checker

--- lsorp_system-v2/server/sv_main.lua
+++ lsorp_system-v2/server/sv_main.lua
@@ -1,7 +1,28 @@
 -- Project: Los Santos Online Roleplay (LSORP)
 -- Author: Gerrit S. | Gerrxt (Admin of LSORP)
--- Last Date: 01.12.2023 (Created the file on 01.12.2023)
+-- Last Date: 02.12.2023 (Created the file on 01.12.2023)
 -- Version: 1.0.0 (Current version)
+-- Official Documentation:
+-- Official Discord:
 -- Script Side: Server
 
-print(Locales[lsorp.language]['script_start'])
+AddEventHandler("onResourceStart", function(resourceName)
+    if GetCurrentResourceName() == resourceName and lsorp.check_for_updates == true then
+        local currentVersion = lsorp.version  -- Nehme die Version aus der Konfigurationsdatei
+
+        PerformHttpRequest("https://api.github.com/repos/Gerrxt07/lsorp/releases/latest", function(responseCode, resultData, resultHeaders)
+            if responseCode == 200 then
+                local releaseData = json.decode(resultData)
+                local latestVersion = releaseData.tag_name
+
+                if currentVersion < latestVersion then
+                    print(Locales[lsorp.language]['script_start_but_old'])
+                else
+                    print(Locales[lsorp.language]['script_start'])
+                end
+            else
+                print(Locales[lsorp.language]['script_no_check'])
+            end
+        end, "GET", "", {})
+    end
+end)
