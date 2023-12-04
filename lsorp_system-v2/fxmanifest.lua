f93d8954194078a44b24755b2e0f30b50d8fdaeb
Gerrit Schlinkmann <gerrxt07@proton.me>
2023-12-04 21:20:22 +0100

    Added Description

--- lsorp_system-v2/fxmanifest.lua
+++ lsorp_system-v2/fxmanifest.lua
@@ -1,8 +1,15 @@
+-- Project: Los Santos Online Roleplay (LSORP)
+-- Author: Gerrit S. | Gerrxt (Admin of LSORP)
+-- Last Date: 01.12.2023 (Created the file on 01.12.2023)
+-- Version: 1.0.0 (Current version)
+-- Official Documentation:
+-- Official Discord:
+
 fx_version 'cerulean'
 game 'gta5'
 
 author 'Gerrxt'
-description 'System für LSORP'
+description 'System for LSORP'
 version '1.0.0'
 
 server_scripts {
