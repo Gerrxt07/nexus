74addf19bdfd25d47a67704635c6a7ef55b2bf8b
Gerrit Schlinkmann <gerrxt07@proton.me>
2023-12-05 21:50:18 +0100

    Added a check for resource name

--- lsorp_system-v2/shared/sh_locales.lua
+++ lsorp_system-v2/shared/sh_locales.lua
@@ -5,15 +5,18 @@
 -- Official Documentation:
 -- Official Discord:
 -- Script Side: Shared
+
 Locales = {
     ['EN'] = {
         ['script_start'] = '^2The Script started successfully and you have the newest version!',
         ['script_start_but_old'] = '^8The Script started successfully, but you have an old version! Please update it!',
         ['script_no_check'] = '^8The Script started successfully, but the version check is not working!',
+        ['script_false_name'] = '^8The script has the wrong name. Please change it to the original one!',
     },
     ['DE'] = {
         ['script_start'] = '^2Das Script wurde erfolgreich gestartet und es ist die aktuellste Version installiert!',
         ['script_start_but_old'] = '^8Das Script wurde erfolgreich gestartet, aber es ist eine veraltete Version installiert! Bitte aktualisiere es!',
         ['script_no_check'] = '^8Das Script wurde erfolgreich gestartet, aber die Versionsüberprüfung funktioniert nicht!',
+        ['script_false_name'] = '^8Das Script hat einen Falschen Namen. Bitte ändere ihn zum Originalen!',
     }
   }
