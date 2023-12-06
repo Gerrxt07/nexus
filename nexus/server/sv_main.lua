4d04e61bbbe337419d872ac5c7ce3c3ea9b552fc
Gerrit Schlinkmann <gerrxt07@proton.me>
2023-12-06 21:07:09 +0100

    updated variable

--- nexus/server/sv_main.lua
+++ nexus/server/sv_main.lua
@@ -8,7 +8,7 @@
 AddEventHandler("onResourceStart", function()
     resourceName = "nexus" -- Real Resource Name, dont edit the name!
     if nexus.check_for_updates == true and GetCurrentResourceName() == resourceName then
-        local currentVersion = nexus.main.version -- Get Current Version, dont edit the version!
+        local currentVersion = nexus.version -- Get Current Version, dont edit the version!
 
         PerformHttpRequest("https://api.github.com/repos/Gerrxt07/nexus/releases/latest", function(responseCode, resultData, resultHeaders) -- Request the latest version from github
             if responseCode == 200 then -- If the request was successfully
@@ -16,15 +16,15 @@ AddEventHandler("onResourceStart", function()
                 local latestVersion = releaseData.tag_name
 
                 if currentVersion < latestVersion then -- Check if the current version is lower than the latest version
-                    print(Locales[nexus.main.language]['script_start_but_old']) -- If check is successfully, but the script is old, print the message!
+                    print(Locales[nexus.language]['script_start_but_old']) -- If check is successfully, but the script is old, print the message!
                 else
-                    print(Locales[nexus.main.language]['script_start']) -- If all checks are successfully, print the message!
+                    print(Locales[nexus.language]['script_start']) -- If all checks are successfully, print the message!
                 end
             else
-                print(Locales[nexus.main.language]['script_no_check']) -- If check was not successfully, print the message!
+                print(Locales[nexus.language]['script_no_check']) -- If check was not successfully, print the message!
             end
         end, "GET", "", {})
     else
-        print(Locales[nexus.main.language]['script_false_name']) -- If the script name is not the same as the real, intended resource name, print the message!
+        print(Locales[nexus.language]['script_false_name']) -- If the script name is not the same as the real, intended resource name, print the message!
     end
 end)
--- nexus/shared/sh_config.lua
+++ nexus/shared/sh_config.lua
@@ -6,13 +6,13 @@
 -- Script Side: Shared
 
 -- Main Config | Please follow the documentation to configurate your script
-nexus.main = {} -- Main config table
+nexus = {} -- Main config table
 
-nexus.main.version = "1.0.0" -- Current Script version | Please do not change this
-nexus.main.check_for_updates = true -- Check for updates on startup (true/false) | Recommend leaving it on
-nexus.main.language = "EN" -- Available languages: EN, DE | More in the future
+nexus.version = "1.0.0" -- Current Script version | Please do not change this
+nexus.check_for_updates = true -- Check for updates on startup (true/false) | Recommend leaving it on
+nexus.language = "EN" -- Available languages: EN, DE | More in the future
 
-nexus.main.admins = { -- Admins table | Please add your steam hex
+nexus.admins = { -- Admins table | Please add your steam hex
     'STEAM_HEX_HERE', -- Example: 'steam:110000143f15cde' | Use https://steamid.pro/ and take the "FiveM, HEX"
 }
 
