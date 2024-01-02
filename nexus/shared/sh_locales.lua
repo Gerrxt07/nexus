dcd21ce666dc8f0c0646a936353016bd0fa57405
Gerrit Schlinkmann <gerrxt07@proton.me>
2024-01-02 15:16:46 +0100

    Added whitelist locales

--- nexus/shared/sh_locales.lua
+++ nexus/shared/sh_locales.lua
@@ -1,5 +1,5 @@
 -- Author: Gerrit S. | Gerrxt
--- Last Date: 27.12.2023 (Created the file on 02.12.2023)
+-- Last Date: 02.01.2024 (Created the file on 02.12.2023)
 -- Version: 1.0.0 (Current version)
 -- Official Documentation:
 -- Official Discord:
@@ -12,12 +12,22 @@ Locales = {
         ['script_no_check'] = '^8The Script started successfully, but the version check is not working!',
         ['script_false_name'] = '^8The script has the wrong name. Please change it to the original one!',
 
+        ['user_whitelist_checking'] = 'Your whitelist is checking...',
+        ['user_whitelist_noidentifier'] = 'There is no Steam ID or Discord Profile found. Please restart FiveM and try again!',
+        ['user_whitelist_nowhitelist'] = 'You are not whitelisted!',
+        ['user_whitelist_canjoin'] = 'Der Server wird geladen!',
+
     },
     ['DE'] = {
         ['script_start'] = '^2Das Script wurde erfolgreich gestartet und es ist die aktuellste Version installiert!',
         ['script_start_but_old'] = '^8Das Script wurde erfolgreich gestartet, aber es ist eine veraltete Version installiert! Bitte aktualisiere es!',
         ['script_no_check'] = '^8Das Script wurde erfolgreich gestartet, aber die Versionsüberprüfung funktioniert nicht!',
         ['script_false_name'] = '^8Das Script hat einen Falschen Namen. Bitte ändere ihn zum Originalen!',
+
+        ['user_whitelist_checking'] = 'Deine Whitelist wird überprüft...',
+        ['user_whitelist_noidentifier'] = 'Es wurde keine Steam ID oder ein Discord Profil gefunden. Bitte starte FiveM neu und versuche es erneut!',
+        ['user_whitelist_nowhitelist'] = 'Du bist nicht Gewhitelistet!',
+        ['user_whitelist_canjoin'] = 'Der Server wird geladen!',
 
     }
   }
