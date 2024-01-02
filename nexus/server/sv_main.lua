7a1022b12e4c3d4544f521e3b07e9b0c0cb3654e
Gerrit Schlinkmann <gerrxt07@proton.me>
2024-01-02 15:16:17 +0100

    Added Whitelist function

--- nexus/server/sv_main.lua
+++ nexus/server/sv_main.lua
@@ -1,5 +1,5 @@
 -- Author: Gerrit S. | Gerrxt
--- Last Date: 27.12.2023 (Created the file on 01.12.2023)
+-- Last Date: 02.01.2024 (Created the file on 01.12.2023)
 -- Version: 1.0.0 (Current version)
 -- Official Documentation:
 -- Official Discord:
@@ -28,4 +28,56 @@ AddEventHandler("onResourceStart", function()
     else
         print(Locales[nexus.language]['script_false_name'])
     end
+end)
+
+AddEventHandler("playerConnecting", function(name, setKickReason, deferrals)
+    local source = source
+    local identifiers = GetPlayerIdentifiers(source)
+
+    if not identifiers or #identifiers == 0 then
+        setKickReason(Locales[nexus.language]['user_whitelist_noidentifier'])
+        CancelEvent()
+        return
+    end
+
+    local steamIdentifier = nil
+    local discordIdentifier = nil
+
+    for _, identifier in ipairs(identifiers) do
+        if string.find(identifier, "discord:") then
+            discordIdentifier = identifier
+        elseif string.find(identifier, "steam:") then
+            steamIdentifier = identifier
+        end
+    end
+
+    if not discordIdentifier or not steamIdentifier then
+        setKickReason(Locales[nexus.language]['user_whitelist_noidentifier'])
+        CancelEvent()
+        return
+    end
+
+    deferrals.defer()
+
+    if nexus.whitelist == true then
+        deferrals.update(Locales[nexus.language]['user_whitelist_checking'])
+
+        MySQL.Async.fetchScalar('SELECT COUNT(*) FROM ' .. nexus.whtelist_table .. ' WHERE steam_id = @steam_id AND discord_id = @discord_id', {
+            ['@steam_id'] = steamIdentifier,
+            ['@discord_id'] = discordIdentifier
+        }, function(result)
+            if result > 0 then
+                Citizen.Wait(1000)
+                deferrals.update(Locales[nexus.language]['user_whitelist_canjoin'])
+                Citizen.Wait(5000)
+                deferrals.done()
+            else
+                deferrals.done(Locales[nexus.language]['user_whitelist_nowhitelist'])
+            end
+        end)
+    else
+        deferrals.done()
+    end
+
+    Wait(0)
 end)
